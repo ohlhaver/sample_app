8dafe89413eff57e809a2336f4ff46b765741dfd
Justus Ohlhaver <ohlhaver@gmail.com>
2012-02-05 13:27:19 +0100

    precompile blueprint assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ SampleApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css)
 
 end
